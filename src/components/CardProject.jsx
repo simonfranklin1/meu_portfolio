279a751d6859ebbb294d24a6cfe87cdc76d9cec9
Simon Franklin <117857690+simonfranklin1@users.noreply.github.com>
2023-12-11 10:43:05 -0300

    Melhorando estilos do Header Alterações nos estilos do header e melhoras na responsividade da página

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -4,7 +4,7 @@ import './CardProject.css'
 const CardProject = ({project}) => {
   return (
     <div className="project-card">
-      <img src={project.image}  className='project-card_image' />
+      <a href={project.deploy} target='_blank' style={{cursor: 'pointer'}}><img src={project.image} style={{width: '100%'}}  className='project-card_image' /></a>
       <div className="project-card_info">
           <h3 className="project-card_info-title" style={{textAlign: 'center'}}>{project.title}</h3>
           <p className="project-card_info-text" style={{textAlign: 'justify', textIndent: '15px'}}>{project.text}</p>
