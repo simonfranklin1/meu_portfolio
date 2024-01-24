5c5c43573940cc834ce3437d71c82d554b7a3e24
Simon Franklin <117857690+simonfranklin1@users.noreply.github.com>
2024-01-24 19:53:40 -0300

    Mudando estilos dos cards de projetos

--- src/components/CardProject.jsx
+++ src/components/CardProject.jsx
@@ -1,20 +1,28 @@
 import React from 'react'
 import './CardProject.css'
+import { RiGitRepositoryLine } from "react-icons/ri";
+import { CgMediaLive } from "react-icons/cg";
 
 const CardProject = ({project}) => {
   return (
     <div className="project-card">
-      <a href={project.deploy} target='_blank' style={{cursor: 'pointer'}}><img src={project.image} style={{width: '100%'}}  className='project-card_image' /></a>
+      <a href={project.deploy} target='_blank'>
+        <img src={project.image} style={{width: '100%'}}  className='project-card_image' />
+      </a>
       <div className="project-card_info">
           <p className="project-card_info-title" style={{textAlign: 'center', fontSize: '20px'}}>{project.title}</p>
           <p className="project-card_info-text" style={{textAlign: 'justify'}}>{project.text}</p>
       </div>
       <div className="project-card_links">
                 <a href={project.repo} target='_blank' >
-                  <button style={{textTransform: 'uppercase'}}>Repositório</button>
+                  <button style={{textTransform: 'uppercase'}} className='project-card_links-repo_btn'>
+                    Repositório <RiGitRepositoryLine />
+                  </button>
                 </a>
                 <a href={project.deploy} target='_blank' >
-                  <button style={{textTransform: 'uppercase'}}>Projeto</button>
+                  <button style={{textTransform: 'uppercase'}} className='project-card_links-demo_btn'>
+                    Site no Ar <CgMediaLive/>
+                  </button>
                 </a>
       </div>
     </div>
